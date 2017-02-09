cf327f0b605b58c7b925e542882a23a0fd9b1f4c
JohnG585 <JGallagher585@Gmail.com>
2017-02-08 21:05:52 -0700

    Add files via upload
    stoplight!

--- index.js
+++ index.js
@@ -2,4 +2,19 @@
   'use strict';
 
   // YOUR CODE HERE
+  var slow = document.querySelector("#slowLight");
+  var slowB = document.querySelector("#slowButton");
+  var stop = document.querySelector("#stopLight");
+  var stopB = document.querySelector("#stopButton");
+  var go = document.querySelector("#goLight");
+  var goB = document.querySelector("#goButton");
+  stopB.addEventListener("click", function() {
+    stop.classList.toggle("stop");
+  });
+  goB.addEventListener("click", function() {
+    go.classList.toggle("go");
+  });
+  slowB.addEventListener("click", function() {
+    slow.classList.toggle("slow");
+  });
 })();
